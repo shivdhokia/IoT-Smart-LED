f1be7225cf16770c2fdcfa9bc7ae3b32124cde8e
Shiv Dhokia <49102640+shivdhokia@users.noreply.github.com>
2020-05-13 15:24:48 +0100

    Update courseworkApp.lua

--- courseworkApp.lua
+++ courseworkApp.lua
@@ -1,4 +1,4 @@
-wifi.sta.sethostname("uopNodeMCU")
+wifi.sta.sethostname("816736NodeMCU")
 wifi.setmode(wifi.STATION)
 station_cfg = {}
 station_cfg.ssid = "TP-Link_7BDB"
@@ -7,8 +7,6 @@ station_cfg.save = true
 wifi.sta.config(station_cfg)
 wifi.sta.connect()
 
--- mytimer = tmr.create()
--- mytimer:register(20000, 1, function() 
 if wifi.sta.getip() == nil then
     print("Connecting to AP...\n")
 else
@@ -20,10 +18,7 @@ else
     print("Gateway Addr: ", gw)
     print("MAC: ", mac)
     print("RSSI: ", rssi, "\n")
-    --    mytimer:stop()
 end
--- end)
--- mytimer:start()
 
 photoPin = 0
 LED_pin = 3
@@ -31,26 +26,27 @@ LED_pin = 3
 gpio.mode(LED_pin, gpio.OUTPUT)
 gpio.write(LED_pin, gpio.LOW)
 
-HOST = "io.adafruit.com" -- adafruit host
-PORT = 1883 -- 1883 or 8883(1883 for default TCP, 8883 for encrypted SSL or other ways)
-ADAFRUIT_IO_USERNAME = "ShivD97" -- put your own username here
-ADAFRUIT_IO_KEY = "aio_GxtH272mDCxHv2u568MlVrl03wzn" -- put your own io_key here
+HOST = "io.adafruit.com"
+PORT = 1883 
+ADAFRUIT_IO_USERNAME = "ShivD97" 
+ADAFRUIT_IO_KEY = "aio_GxtH272mDCxHv2u568MlVrl03wzn"
 PUBLISH_TOPIC = "ShivD97/feeds/stream"
-SUBSCRIBE_TOPIC = "ShivD97/feeds/brightnessled" -- put your topic of subscribe shown on the IoT platform/broker site
--- init mqtt client with logins, keepalive timer 300 seconds
+SUBSCRIBE_TOPIC = "ShivD97/feeds/brightnessled" 
+
+
 m = mqtt.Client("Client1", 300, ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
--- setup Last Will and Testament (optional)
--- Broker will publish a message with qos = 1, retain = 0, data = "offline"
--- to topic "/lwt" if client does not send keepalive packet
+
+
 m:lwt("/lwt", "Now offline", 1, 0)
--- on different event "connect","offline","message",...
+
+
 m:on("connect", function(client)
     print("Client connected")
     print("MQTT client connected to " .. HOST)
     client:subscribe(SUBSCRIBE_TOPIC, 1,
                      function(client) print("Subscribe successfully") end)
 
-    pubADC(client)
+    pubBrightness(client)
 end)
 
 m:on("offline", function(client) print("Client offline") end)
@@ -60,11 +56,11 @@ pinDim = 3
 pinADC = 0
 
 mytimer = tmr.create()
-print("Timer Created")
+
+
 mytimer:register(5000, 1, function()
     digitV = adc.read(pinADC)
-    print("Light")
-    print(digitV)
+
     if (digitV < 10) then
         dc = digitV * 100
     else
@@ -74,9 +70,6 @@ mytimer:register(5000, 1, function()
 end)
 
 m:on("message", function(client, topic, data)
-    print(topic .. ":")
-    print("data")
-    print(data)
 
     pwm.setup(pinDim, 1000, dc)
     pwm.start(pinDim)
@@ -118,7 +111,7 @@ m:on("message", function(client, topic, data)
     end
 end)
 
-function pubADC(client)
+function pubBrightness(client)
     mytimerPublish = tmr.create()
     mytimerPublish:register(5000,1,function()
 
